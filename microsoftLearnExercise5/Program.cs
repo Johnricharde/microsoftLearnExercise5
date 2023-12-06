e072fe85c569fd53cd9113ec23b0772fc64add1b
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2023-12-06 12:59:01 +0100

    Add project files.

---
+++ microsoftLearnExercise5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.Tracing;
+
+namespace microsoftLearnExercise5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pangram = "The quick brown fox jumps over the lazy dog";
+
+            string[] pangramArray = pangram.Split(' ');
+            string stringResult = "";
+
+            foreach (string word in pangramArray)
+            {
+                char[] wordArray = word.ToCharArray();
+                wordArray.Reverse();
+                stringResult += String.Join("", wordArray.Reverse()) + " ";
+            }
+
+
+            Console.WriteLine(stringResult);
+        }
+    }
+}
